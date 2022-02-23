25f5c79dcbbdd90fcc1473139b492e5cf83d4c1b
Antonio de Jesus <49320018+AntonioDJSS@users.noreply.github.com>
2022-02-22 22:37:14 -0600

    Update AreaCadradoTriangualo.py

--- AreaCadradoTriangualo.py
+++ AreaCadradoTriangualo.py
@@ -36,3 +36,30 @@ print(suma)
 
 entradaTeclado = input('Hola bb')
 print(entradaTeclado)
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+#Realice un programa que calcule el area de un triangulo y el area de un cuadrado.
+
+print('¿Que quieres calcular?')
+dato = int(input('\nArea de un triangulo (Selecciona 1)\nArea del cuadrado: (Selecciona 2): '))
+
+def calculadoraTriangulo(base, altura, areaTriangulo):
+  base = float(input('\nInserta la base de tu triangulo: '))
+  altura = float(input('Inserta la altura de tu triangulo: '))
+  areaTriangulo = (base * altura) / 2
+  print('La base de tu triangulo es: {}'.format(areaTriangulo))    
+  return
+
+def calculadoraCuadrado(baseCuadrado, multi):
+  baseCuadrado = int(input('\nInserta un lado de tu cuadrado: '))
+  multi  = (baseCuadrado * baseCuadrado)
+  print('El area de tu triangulo es: {}'.format(multi)) 
+  return
+
+if dato == 1:
+  print(calculadoraTriangulo(base, altura, areaTriangulo))
+elif dato == 2:
+  print(calculadoraCuadrado(baseCuadrado, multi))
+else:
+  print('\nInsertaste mal el dato')
+
+
