d83c4c385280d5d7a2ef7b845ecde09ce1861c2c
Antonio de Jesus <49320018+AntonioDJSS@users.noreply.github.com>
2022-02-22 22:31:51 -0600

    Create AreaCadradoTriangualo.py

---
+++ AreaCadradoTriangualo.py
@@ -0,0 +1,38 @@
+#Variables
+entero = 45
+flotante = 10.2
+cadena = 'Jesus'
+boleano = True
+
+#Conversion de datos
+entero = int(flotante)
+print(entero)
+flotante = float(entero)
+print(entero)
+Cadena2 = str(45)
+print(Cadena2)
+
+hola = type(Cadena2)
+print(hola)
+
+print('Esto es una cadena')
+print('Hola mundo')
+print(entero)
+print(flotante)
+print(cadena)
+print(boleano)
+
+#Operadores Aritmeticos
+# + - * / % **
+
+print(2 + 2)
+print(3 - 5)
+print(3 * 3)
+print(4 % 3)
+print(3 ** 3)
+
+suma = 3 + 3
+print(suma)
+
+entradaTeclado = input('Hola bb')
+print(entradaTeclado)
